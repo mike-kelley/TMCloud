e87ce6ff7524468c6c7255204b6702a1aa5ca56d
Mike Kelley <mk00340851@techmahindra.com>
2015-10-16 13:08:43 -0500

    updated permissions on basic_env files Fri Oct 16 13:08:43 CDT 2015

--- basic_env1.sh
+++ basic_env1.sh
@@ -27,8 +27,8 @@ for i in $(seq 1 $count); do
 echo "export $(head -$i identities | tail -1)=$(openssl rand -hex 12)" >> passwords
 done
 
-sudo cp hosts/$(hostname).hosts  /etc/hosts
-sudo cp interfaces/$(hostname).interfaces /etc/network/interfaces
+#sudo cp hosts/$(hostname).hosts  /etc/hosts
+#sudo cp interfaces/$(hostname).interfaces /etc/network/interfaces
 
 ping -c3 openstack.org
         if [ $? -ne 0 ]; then
@@ -44,30 +44,30 @@ done
 
 sudo apt-get -y install ntp
 
-sudo cp $(hostname).ntp.conf /etc/ntp.conf
-sudo rm /var/lib/ntp/ntp.conf.dhcp
-sudo service ntp restart
-sudo apt-get install ubuntu-cloud-keyring
-sudo cp kilo.lst /etc/apt/sources.list.d/cloudarchive-kilo.list
-sudo apt-get update && apt-get upgrade
-sudo reboot
-
-Database server (controller)
-	apt-get install mysql-server python-mysqldb
-		use database root password from password.ods
-	sudo vim /etc/mysql/conf.d/mysqld_openstack.cnf
-		[mysqld]
-		...
-		bind-address = 10.0.0.2
-		default-storage-engine = innodb
-		innodb_file_per_table
-		collation-server = utf8_general_ci
-		init-connect = 'SET NAMES utf8'
-		character-set-server = utf8
-	# service mysql restart
-	# mysql_secure_installation
-
-Message Queue (controller)
+#sudo cp $(hostname).ntp.conf /etc/ntp.conf
+#sudo rm /var/lib/ntp/ntp.conf.dhcp
+#sudo service ntp restart
+#sudo apt-get install ubuntu-cloud-keyring
+#sudo cp kilo.lst /etc/apt/sources.list.d/cloudarchive-kilo.list
+#sudo apt-get update && apt-get upgrade
+#sudo reboot
+#
+#Database server (controller)
+#	apt-get install mysql-server python-mysqldb
+#		use database root password from password.ods
+#	sudo vim /etc/mysql/conf.d/mysqld_openstack.cnf
+#		[mysqld]
+#		...
+#		bind-address = 10.0.0.2
+#		default-storage-engine = innodb
+#		innodb_file_per_table
+#		collation-server = utf8_general_ci
+#		init-connect = 'SET NAMES utf8'
+#		character-set-server = utf8
+#	# service mysql restart
+#	# mysql_secure_installation
+#
+#Message Queue (controller)
 	# apt-get install rabbitmq-server
 	# rabbitmqctl add_user openstack RABBIT_PASS (from password.ods)
 	# rabbitmqctl set_permissions openstack ".*" ".*" ".*"
